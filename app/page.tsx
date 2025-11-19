dbcce9a09dd91c76535be983e72775b521a8f7e3
RIKU <riku@RIKUnoMacBook-Pro.local>
2025-11-19 19:07:48 +0900

    W0: FINAL PUSH - Hello Confirmed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-// app/page.tsx に貼り付けるコード
 import React from 'react';
-
-// Home コンポーネントを定義します
 const Home = () => {
-  // 画面に表示する内容を返します
   return (
     <div style={{
       display: 'flex',
@@ -18,6 +14,4 @@ const Home = () => {
     </div>
   );
 };
-
-// Home コンポーネントをエクスポート（利用可能に）します
 export default Home;
